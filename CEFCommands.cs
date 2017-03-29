1c7d3190fa6396fc8672a03041c2f1678eca80a6
ImToro <kiwiboytrent2@gmail.com>
2017-03-29 18:14:33 +0800

    Create CEFCommands.cs

---
+++ CEFCommands.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using GTANetworkShared;
+
+namespace CEFCommands
+{
+
+    public class CEFCommands : Script
+    {
+        public CEFCommands()
+        {
+            API.onClientEventTrigger += OnClientTriggered;
+        }
+
+        [Command("test")]
+        public void TestFunction(Client player, string eventName, params object[] arguments)
+        {
+            API.triggerClientEvent(player, "testjs");//will run the browser
+        }
+        public void OnClientTriggered(Client player, string eventName, params object[] arguments)//Links to when the browser btnclicked happens
+        {
+            switch (eventName)
+            {
+                case "BackToTheFuture":
+                    API.sendNotificationToPlayer(player, "We're back where we started.");
+                    break;
+            }
+        }
+    }
+
+}
