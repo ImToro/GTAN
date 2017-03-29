58561aafbfec8516834418ff7e7f7c9a78f2dfa6
ImToro <kiwiboytrent2@gmail.com>
2017-03-29 19:55:33 +0800

    Update CEF.js

--- CEF.js
+++ CEF.js
@@ -7,7 +7,7 @@ API.onServerEventTrigger.connect((eventName, args) => {
 });
 function TestHandler() {
     var res = API.getScreenResolution(); // Gets screen res
-    Browser = API.createCefBrowser(500, 499); // size of window
+    browser = API.createCefBrowser(500, 499); // size of window
     API.waitUntilCefBrowserInit(browser); // Waits for client to load it
     API.serCefBrowserPosition(browser, (res.Width / 2) - (500 / 2), (res.height / 2) - (499 / 2)); //Sets the point of the browser
     API.loadPageCefBrowser(browser, "Designs/Blackbox.html");//Directs to the page to load, make sure full directory
